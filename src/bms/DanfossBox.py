44c99fcb767543b7c3e4f829b395ff6a6b58785d
Henry Hernaiz <hernaizhenry@gmail.com>
2025-11-11 14:02:10 -0800

    fix: bug when removing cleared alarms from virtual alarm points

--- src/bms/DanfossBox.py
+++ src/bms/DanfossBox.py
@@ -362,6 +362,10 @@ class DanfossBox:
                         for k, v in details.items()
                         if k not in ["@nodetype", "@node", "@mod", "@point"]
                     }
+                    final["@nodetype"] = f"alarm_{ref}"
+                    final["@node"] = f"alarm_{ref}"
+                    final["@mod"] = f"alarm_{ref}"
+                    final["@point"] = f"alarm_{ref}"
                 await self.add_nodetype(final)
 
         active_refs = alarm_references.get("active", {}).get("ref", None)
